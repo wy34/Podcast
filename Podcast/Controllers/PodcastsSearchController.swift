422619ccc0c8770fcc71030256807807d7847f54
William Yeung <williamyeung1141998@gmail.com>
2020-11-02 11:40:39 -0500

    Created podcast model and setup basic podCastSearchController table with cells

--- Podcast/Controllers/PodcastsSearchController.swift
+++ Podcast/Controllers/PodcastsSearchController.swift
@@ -8,10 +8,40 @@
 import UIKit
 
 class PodcastsSearchController: UITableViewController {
+    // MARK: - Properties
+    let podcasts = [
+        Podcast(name: "Lets Build That App", artist: "Brian Voong"),
+        Podcast(name: "Some Podcast", artist: "Some Author")
+    ]
+    
+    let cellId = "cellId"
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTableView()
+    }
+    
+    // MARK: - Helpers
+    func configureTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+    }
+}
+
+// MARK: - Extension
+extension PodcastsSearchController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return podcasts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
         
+        let podcast =  podcasts[indexPath.row]
+        cell.textLabel?.text = "\(podcast.name)\n\(podcast.artist)"
+        cell.textLabel?.numberOfLines = -1
+        cell.imageView?.image = #imageLiteral(resourceName: "appicon")
+        
+        return cell
     }
 }
---
+++ Podcast/Models/Podcast.swift
@@ -0,0 +1,13 @@
+//
+//  Podcast.swift
+//  Podcast
+//
+//  Created by William Yeung on 11/2/20.
+//
+
+import Foundation
+
+struct Podcast {
+    let name: String
+    let artist: String
+}
