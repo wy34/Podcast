4b6bd90d5b71a5f1976c81227164e91c488ffd06
William Yeung <williamyeung1141998@gmail.com>
2020-11-01 20:29:33 -0500

    First commit

---
+++ Podcast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Podcast
+//
+//  Created by William Yeung on 11/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
